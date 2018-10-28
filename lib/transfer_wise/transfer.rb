e671a20eb33dda14e35692aa28d18e5b3d25b52a
James Bradley <james@ongosa.com>
2018-10-28 16:33:47 +0000

    Add payment action

--- lib/transfer_wise/transfer.rb
+++ lib/transfer_wise/transfer.rb
@@ -1,4 +1,9 @@
 module TransferWise
   class Transfer < APIResource
+    def create_payment(headers = {})
+      url = self.class.resource_url(id) + "/payments"
+      response = TransferWise::Request.request(:post, url, { type: 'BALANCE' }, headers)
+      response['status'] == 'COMPLETED'
+    end
   end
 end
